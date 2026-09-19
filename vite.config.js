3c83daaed6e689a4eb7b5ccf6f7257e37ac5e1ba
Angry548 <angelmate827@gmail.com>
2026-09-18 22:00:34 -0600

    Correccion de errores

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Practica20260912/', // Debe ser exactamente /Practica20260912/ con P mayúscula
 })
