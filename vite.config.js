11e445c96fccb2e7f5249712796e2c46b6e909d8
Angry548 <angelmate827@gmail.com>
2026-09-18 21:39:16 -0600

    fix: cambiar base a / para Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Practica20260912/', // Debe ser exactamente /Practica20260912/ con P mayúscula
+  base: '/', // Debe ser '/' para Vercel
 })
